c3668d8b702ad1dafd97aaa0775c43f01120197a
ErikaPOrtiz <erika0418p@gmail.com>
2025-04-02 15:05:01 -0600

    update test again

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('Erika Ortiz ID 456980', () => {
   render(<App />);
-  const linkElement = screen.getByText(/MY CIDC/i);
+  const linkElement = screen.getByText(/Erika Ortiz ID 456980/i);
   expect(linkElement).toBeInTheDocument();
 });
